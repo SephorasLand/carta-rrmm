6637227734c7d4b5bf468cf395af9959d3c8887c
Raquel Pereda <rperedah@gmail.com>
2020-12-05 08:42:42 +0100

    Changes in the text of the button and its logic. Changes in color. Removed unused pictures

--- index.js
+++ index.js
@@ -1,6 +1,5 @@
 var i = 0;
 var txt = 'Queridos Reyes Magos,'; /* The text */
-var txt2 = '¡¡¡¡Gracias!!!!'
 var speed = 70; /* The speed/duration of the effect in milliseconds */
 
 function typeWriter() {
@@ -11,17 +10,10 @@ function typeWriter() {
   }
 }
 
-function typeWriter2() {
-  if (i < txt.length) {
-    document.getElementById("title2").innerHTML += txt2.charAt(i);
-    i++;
-    setTimeout(typeWriter, speed);
-  }
-}
-
 function areYouSure() {
   if(document.getElementById("textoCambiante").innerHTML == "¿Aceptáis el acuerdo?"){
     document.getElementById("textoCambiante").innerHTML = "¿Estáis seguros?";
+    document.getElementById("boton").value = "Estamos seguros";
   } else if(document.getElementById("boton").value == "¡QUE SÍ, PESADA!") {
     window.open("https://www.amazon.es/Yamaha-P-125B-Piano-digital-color/dp/B07BZTZC6R/ref=sr_1_1?__mk_es_ES=%C3%85M%C3%85%C5%BD%C3%95%C3%91&dchild=1&keywords=yamaha+p125b&qid=1607075744&sr=8-1");
   } else {
@@ -31,4 +23,3 @@ function areYouSure() {
 }
 
 typeWriter();
-typeWriter2();
